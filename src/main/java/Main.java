db9494d227745c37b3bd76c374aac9a1e7c4c81b
waiter-palypoo <gohjinghong2000@gmail.com>
2023-01-20 17:54:39 +0800

    Added greeting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+    // hope you enjoyed!
     public static void main(String[] args) {
         LoanManager m1 = new LoanManager();
         m1.addLoan("Alice Betsy", 10, 0.1);
